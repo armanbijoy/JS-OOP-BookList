5c209a655b78dd2f8f1e8c649e6acb211349e2f8
Arman Bijoy <armanbijoy4@gmail.com>
2022-01-18 21:32:40 +0600

    delete functionality added

--- app.js
+++ app.js
@@ -32,6 +32,15 @@ UI.prototype.showAlert = function (message, classname) {
     document.querySelector('.alert').remove()
   }, 3000)
 }
+
+//Delete Book
+
+UI.prototype.deleteBook = function (target) {
+  if (target.className === 'delete') {
+    target.parentElement.parentElement.remove()
+  }
+}
+
 UI.prototype.clearfields = function () {
   document.getElementById('title').value = ''
   document.getElementById('author').value = ''
@@ -62,5 +71,15 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
     ui.clearfields()
   }
 
+  e.preventDefault()
+})
+
+//Event Listner for delete
+
+document.getElementById('book-list').addEventListener('click', function (e) {
+  const ui = new UI()
+  ui.deleteBook(e.target)
+
+  ui.showAlert('Book Removed', 'success')
   e.preventDefault()
 })
